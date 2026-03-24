6b512c5da91978fe2ab4139bc5d28153edc4b17a
Xicola68 <ranndoneur26@gmail.com>
2026-03-24 19:20:01 +0100

    style: switch from SVG to PNG logos in header and UI

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,12 +4,12 @@ const Logo = ({ className = "h-8" }) => {
     return (
         <div className="flex items-center">
             <img
-                src="/images/logo_killer_control_pos.svg"
+                src="/images/killer_control_pos.png"
                 alt="Killer Control"
                 className={`${className} w-auto block dark:hidden`}
             />
             <img
-                src="/images/logo_killer_control_neg.svg"
+                src="/images/killer_control_neg.png"
                 alt="Killer Control"
                 className={`${className} w-auto hidden dark:block`}
             />
